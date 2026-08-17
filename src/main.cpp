1a24981a76895a59803d0097477bf37e1e34e22d
Ted Chang <taedlar@gmail.com>
2026-08-18 00:52:33 +0800

    feat: link with argparse

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,26 @@
 #include "config.h"
+#include <argparse/argparse.hpp>
 #include <mudmux/mudmux.h>
 
-int main() {
-    int exit_code = 0;
+static void process_command_line(int argc, char* argv[]);
+
+int main (int argc, char* argv[]) {
+    int exit_code = EXIT_SUCCESS;
+    process_command_line(argc, argv);
     if (mudmux_init("") == 0) {
         mudmux_deinit();
     }
+    SPDLOG_INFO("exiting ({})", exit_code);
     return exit_code;
+}
+
+void process_command_line(int argc, char* argv[]) {
+    argparse::ArgumentParser program (argv[0], "1.0");
+    try {
+        program.parse_args(argc, argv);
+    }
+    catch (const std::runtime_error& err) {
+        std::cerr << err.what() << std::endl << program;
+        std::exit(EXIT_FAILURE);
+    }
 }
